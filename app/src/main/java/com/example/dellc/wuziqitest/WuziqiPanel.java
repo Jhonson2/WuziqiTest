8b84e66c8354adcc9e10311302ab7b3682eb4a01
Jhonson2 <1750896420@qq.com>
2017-06-08 10:41:34 +0800

    新建一个view——WuziqiPanel类+xml布局的编写

---
+++ app/src/main/java/com/example/dellc/wuziqitest/WuziqiPanel.java
@@ -0,0 +1,18 @@
+package com.example.dellc.wuziqitest;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by dellc on 2017/6/8.
+ */
+
+public class WuziqiPanel extends View {
+
+    public WuziqiPanel(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        setBackgroundColor(0x44ff0000);
+    }
+}
